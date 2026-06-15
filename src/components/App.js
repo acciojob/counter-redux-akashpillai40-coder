8b1e78cc9e1670388866e27cfbefc2941c0c3e8d
Akash S Pillai <akashpillai40@gmail.com>
2026-06-15 09:55:01 +0530

    feat: implement redux state management and fix compiler plugins

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../store/counterSlice";
 
 const App = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch()
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
